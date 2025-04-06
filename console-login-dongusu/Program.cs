52624eba018800631102eebb0dd10db382fd9f43
Kordyss99 <kordyss99@outlook.com>
2025-04-06 23:20:43 +0300

    console-login

---
+++ console-login-dongusu/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace console_login_dongusu
+{
+    internal class Programd
+    {
+        static void Main(string[] args)
+        {
+            string username = "1";
+            string pass = "1";
+            string kad, ksif;
+
+            Console.WriteLine("Kullanıcı Doğrulanıyor");
+
+            int deneme = 0;
+            bool kontrol = false;
+
+            while (deneme <= 3 && !kontrol) 
+            {
+                
+                Console.Write("Kullanıcı Adı: ");
+                kad = Console.ReadLine();
+                Console.Write("Kullanıcı Sifre: ");
+                ksif = Console.ReadLine();
+
+                if (username == kad && pass == ksif)
+                {
+                    kontrol = true;
+                    Console.WriteLine("Giriş Başarılı");
+                }
+                else
+                {
+                    deneme++;
+                    Console.WriteLine("Yanlış Girdiniz");
+                }
+            }
+            if (kontrol)
+            {
+                Console.WriteLine("Kordyss99");
+            }
+            else
+            {
+                kontrol = false;
+                Console.WriteLine("programdan gidin");
+            }
+            
+        }
+    }
+}
